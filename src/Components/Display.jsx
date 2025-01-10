e75c33f53dbfccc2d46f306d28e2d70749c66d3b
paramaguru077 <gurucse077@gmail.com>
2025-01-10 13:16:40 +0530

    React tailwind project

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,31 +1,54 @@
 import React from 'react'
 
-const Display = ({ submittedData }) => {
+const Display = ({ submittedData , handleDelete}) => {
   return (
-    <div className="overflow-x-auto mt-4">
-      {submittedData && submittedData.length ? (
-        <table className="min-w-full table-auto border-collapse border border-blue-300 shadow-md">
-          <thead className="bg-blue-100">
-            <tr>
-              <th className="py-2 px-4 text-left font-semibold text-blue-700 border-b border-blue-300">Name</th>
-              <th className="py-2 px-4 text-left font-semibold text-blue-700 border-b border-blue-300">Email</th>
-              <th className="py-2 px-4 text-left font-semibold text-blue-700 border-b border-blue-300">Message</th>
-            </tr>
-          </thead>
-          <tbody>
-            {submittedData.map((val, i) => (
-              <tr key={i} className="hover:bg-blue-50">
-                <td className="py-2 px-4 border-b border-blue-300">{val.name}</td>
-                <td className="py-2 px-4 border-b border-blue-300">{val.email}</td>
-                <td className="py-2 px-4 border-b border-blue-300">{val.message}</td>
+    <>
+      <div className="overflow-auto rounder-lg shadow  w-full hidden  sm:flex justify-center">
+        {submittedData && submittedData.length ? (
+          <table className="w-8/12  sm:10/12 m-4 table-auto border-collapse border border-blue-300 shadow-md">
+            <thead className="bg-blue-100">
+              <tr>
+                <th className="py-2 px-4 text-left font-semibold text-blue-700 border-b border-blue-300">Name</th>
+                <th className="py-2 px-4 text-left font-semibold text-blue-700 border-b border-blue-300">Email</th>
+                <th className="py-2 px-4 text-left font-semibold text-blue-700 border-b border-blue-300">Message</th>
+                <th className="py-2 px-4 text-left font-semibold text-blue-700 border-b border-blue-300">Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <p className="text-blue-600">No data available</p>
-      )}
-    </div>
+            </thead>
+            <tbody className='divide-y divide-gray-400' >
+              {submittedData.map((val, i) => (
+                <tr key={i} className="hover:bg-blue-50">
+                  <td className="py-2 px-4 border-b border-blue-300 font-serif">{val.name}</td>
+                  <td className="py-2 px-4 border-b border-blue-300">{val.email}</td>
+                  <td className="py-2 px-4 border-b border-blue-300">{val.message}</td>
+                  <button className='px-3 bg-red-600 text-md text-white m-1 border-1 border-red-500 rounded-xl ring-1 active:bg-red-500' onClick={()=>handleDelete(i)}>Delete</button>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p className="text-blue-600">No data available</p>
+        )}
+      </div>
+      <div className=' container x-auto  grid grid-cols-1  gap-4 sm:hidden'>
+        {
+          submittedData.map((val,i)=>(
+             <div className=" bg-white p-4 rounded-lg shadow border-2 border-indigo-200 m-4 " key={i}>
+              <div className='flex flex-col items-center space-x-2 text-sm'>
+                <div className='text-left font-serif text-lg text-gray-500 hover:underline uppercase'>{val.name}</div>
+                <div className='text-lg text-gray-600 tracking-wide'>{val.email}</div>
+                <div className='text-lg text-gray-700 tracking-wide'>{val.message}</div>
+                <div><button className='px-3 bg-red-600 text-md text-white m-1 border-1 border-red-500 rounded-xl ring-1 active:bg-red-500' onClick={()=> handleDelete(i)}>Delete</button></div>
+
+              </div>
+
+
+              </div>
+          ))
+        }
+
+
+      </div>
+    </>
   )
 }
 
--- src/Components/UserData.jsx
+++ src/Components/UserData.jsx
@@ -8,24 +8,59 @@ const UserData = () => {
     message: '',
   });
   const[submittedData,setSubmittedData] = useState([]);
+  const [error,setError]= useState({});
+  
 
-  const handleSubmit = () => {
+  const validate =()=>{
+    let isValidate ={};
+    if(data.name.trim()===""){
+      isValidate.name="name is required";
+    }
+    const reg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(data.email===""){
+      isValidate.email="Email is required";
+    }
+    else if(!reg.test(data.email)){
+       isValidate.email="Enter validate email";
+    }
+    if(data.message.trim()===""){
+      isValidate.message="Message is reqired";
+
+    }
+    return isValidate;
+  };
+
+  const handleDelete = (index)=>{
+    setSubmittedData((prevData)=> prevData.filter(
+      (_,i)=> index!==i
+    ));
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
      
+    const checkValidate  =  validate();
+    if(Object.keys(checkValidate).length>0){
+      setError(checkValidate);
+      
 
-    setSubmittedData((prevData)=> [...prevData,data])
-    
-
-
+    }
+    else{
+      setError({});
+      setSubmittedData((prevData)=> [...prevData,data])
     setData({
       name: '',
       email: '',
       message: '',
     });
+    }
+
+    
   };
 
-  const handleForm = (e) => {
-    e.preventDefault();
-  };
+  
+  
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -37,15 +72,16 @@ const UserData = () => {
   
 
   return (
-    <div className="bg-gray-100 container mx-auto flex flex-col justify-center items-center py-12 mt-6 rounded-lg shadow">
+    <>
+    <div className="bg-gray-100 container mx-auto flex flex-col justify-center items-center py-12 mt-6 rounded-lg shadow w-[90%] sd:w-full">
       <h1 className="font-serif text-blue-500 text-lg mb-3 font-bold">
         Enter Your Details
       </h1>
-      <p className="m-1 text-center text-sm hover:underline text-blue-600 p-1">
-        We'd love to hear from you! Fill out your details and drop us a message. We'll get back to you as soon as possible.
+      <p className="m-1 text-center text-sm hover:underline text-blue-600 p-1 font-medium">
+         Fill out your details.
       </p>
-      <form onSubmit={handleForm} className="m-2 p-4 bg-gray-50 rounded-xl shadow  flex flex-col items-center">
-        <div className="m-2 flex gap-1">
+      <form onSubmit={handleSubmit} className="m-2 p-4 bg-gray-50 rounded-xl shadow  flex flex-col items-center w-full sm:w-3/4 md:w-1/2 lg:w-1/3">
+        <div className="m-2 flex    gap-1">
           <label htmlFor="name" className="text-blue-800 text-[18px] font-serif">
             Name:
           </label>
@@ -56,6 +92,9 @@ const UserData = () => {
             value={data.name}
             className="input-flied"
           />
+         <div >
+         {error.name && <p className='text-red-700 text-[13px]'> {error.name}</p>}
+         </div>
         </div>
 
         <div className="flex gap-1">
@@ -69,39 +108,43 @@ const UserData = () => {
             value={data.email}
             className="input-flied"
           />
+          {error.email && <p className='text-red-700 text-[13px]'> {error.email}</p>}
         </div>
 
         <div className="flex gap-1">
           <label htmlFor="message" className="text-blue-800 text-[18px] font-serif">
             Message:
           </label>
-          <textarea
+          <textarea style={{maxWidth:'300px', maxHeight:"150px"}}
             name="message"
             value={data.message}
             onChange={handleChange}
             className="input-flied"
           ></textarea>
+          {error.message && <p className='text-red-700 text-[13px]'> {error.message}</p>}
         </div>
-      </form>
-      <div className="mt-4">
+        <div className="mt-4">
         <button
           className="px-6 py-2 bg-indigo-600 rounded-lg text-blue-200 font-mono text-[18px] hover:bg-indigo-500 active:bg-blue-700"
           onClick={handleSubmit}
         >
           Submit
         </button>
-      </div>
-
-      {submittedData.length>0 && (
+      </div> 
+      </form>
+      
+    </div>
+    {submittedData && (
           <>
             
-         <div class="w-[70%] my-4 h-[2px] bg-gray-300 shadow-md rounded-lg"></div>
+         <div class="w-[50%] my-4 ml-[25%] h-[2px] bg-gray-300 shadow-md rounded-lg flex justify-center"></div>
 
-         <Display submittedData={submittedData} />
+         <Display submittedData={submittedData} handleDelete={handleDelete} />
           
         </>
       )}
-    </div>
+    </>
+
   );
 };
 
